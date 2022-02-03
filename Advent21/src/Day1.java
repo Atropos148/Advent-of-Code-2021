220017471f28c8c065433aa9ffb3efe862209a97
atropos148 <tibor.h148@gmail.com>
2022-02-03 14:28:38 +0100

    Setup for easier handling of per day code

---
+++ Advent21/src/Day1.java
@@ -0,0 +1,5 @@
+public class Day1 {
+	public void testPrint() {
+		System.out.println("Hello World");
+	}
+}
---
+++ Advent21/src/Manager.java
@@ -0,0 +1,7 @@
+public class Manager {
+	public static void main(String[] args) {
+		Day1 day1 = new Day1();
+
+		day1.testPrint();
+	}
+}
