68b4c6ee3feaae6d52c49aca57c712b3e5dea772
atropos148 <tibor.h148@gmail.com>
2022-02-03 15:40:06 +0100

    Day 1 - Part 1 & 2

--- Advent21/src/Manager.java
+++ Advent21/src/Manager.java
@@ -2,6 +2,7 @@ public class Manager {
 	public static void main(String[] args) {
 		Day1 day1 = new Day1();
 
-		day1.testPrint();
+		day1.countDepthDrops();
+		day1.countSlidingWindowDrops();
 	}
 }
